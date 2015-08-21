dd3ffada1d10d353acace20eb15c0641fbec8579
Harshal Sheth <hsheth2@gmail.com>
2015-08-21 16:16:33 -0400

    Bug fixes and foreman setup. Added a loading message to the search feature

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,5 +1,6 @@
 module MainHelper
     def processLinks(str)
-        str.gsub('/((https?:)?\\/\\/[^\\s]+)/', '<a style="font-weight: bold;" href="\\1" target="_BLANK">\\1</a>')
+        n = str.gsub(/((https?:)?\/\/[^\s]+)/, '<a style="font-weight: bold;" href="\1" target="_BLANK">\1</a>')
+        n.html_safe
     end
 end
